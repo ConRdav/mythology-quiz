7c5841ab6ccc4553490ce7cff70bb361e48f1ec7
ConRdav <connor@redwoodglade.com>
2021-11-30 15:18:26 +0000

    Added JS file to store questions

---
+++ assets/js/questions.js
@@ -0,0 +1,113 @@
+// This is used to store the questions for the quiz
+let questions = [
+    {//Question 1
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 2
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 3
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 4
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 5
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 6
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 7
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 8
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 9
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+    {//Question 10
+        question:"Who is the King of the Gods in Greek Mythology?",
+        correctAnswer:"Zeus",
+        answers:
+        [
+            "Zeus",
+            "Hades",
+            "Posiedon",
+            "Appolo"
+        ]
+    },
+]
