2fbc5fb05cef03cfa5a237c85ab7fda8483a5590
ConRdav <connor@redwoodglade.com>
2021-12-05 18:46:47 +0000

    Added hide class to hide containers until activated by JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
-//all elements
-const mortalBtn = document.getElementById("mortal-btn")
-const demiBtn = document.getElementById("demi-btn")
-const olympianBtn = document.getElementById("olympian-btn")
-const mortalRules = document.getElementById("mortal-rules")
-const demiRules = document.getElementById("demi-rules")
-const olympianRules = document.getElementById("olympian-rules")
-const quitBtn = document.getElementsByClassName(".rules-footer .quit-btn")
-const startBtn = document.getElementsByClassName(".rules-footer .start-btn")
+
 
 function startGame() {
 
