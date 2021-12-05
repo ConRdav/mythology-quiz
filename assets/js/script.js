354b983840d8a16a763af690108d25b0f1a3ff68
ConRdav <connor@redwoodglade.com>
2021-12-05 19:05:53 +0000

    Added JS for start game for mortal difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
+const mortalGame = document.getElementById("mortal-start-btn")
+const demiGame = document.getElementById("demi-start-btn")
+const olympianGame = document.getElementById("olympian-start-btn")
+const mortalRules = document.getElementById("mortal-rules")
+const demiRules = document.getElementById("demi-rules")
+const olympianRules = document.getElementById("olympian-rules")
+const quizGame = document.getElementById("quiz-container")
+
+mortalGame.addEventListener('click', startGameMortal)
+demiGame.addEventListener('click', startGameDemi)
+olympianGame.addEventListener('click', startGameOlympian)
 
 
-function startGame() {
+function startGameMortal() {
+    mortalRules.classList.add('hide')
+    quizGame.classList.remove('hide')
+    setNextQuestion()
+
+    
+}
+
+function startGameDemi() {
 
 }
 
+function startGameOlympian() {
+
+}
 function setNextQuestion() {
 
 }
