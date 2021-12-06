065118582220a61cae16b6fe9a5385cd475e6c5c
ConRdav <connor@redwoodglade.com>
2021-12-06 14:13:54 +0000

    Added JS event listeners when next button clicked and answer clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,53 @@
 // Collecting all elements needed
+
+//Start game button
 const mortalGame = document.getElementById("mortal-start-btn")
 const demiGame = document.getElementById("demi-start-btn")
 const olympianGame = document.getElementById("olympian-start-btn")
+
+//Difficulty selector buttons
 const mortalDifficulty = document.getElementById("mortal-btn")
 const demiDifficulty = document.getElementById("demi-btn")
 const olympianDifficulty = document.getElementById("olympian-btn")
+const difficultyTitle = document.getElementById("difficulty-title")
+
+//Difficulty rules
 const mortalRules = document.getElementById("mortal-rules")
 const demiRules = document.getElementById("demi-rules")
 const olympianRules = document.getElementById("olympian-rules")
+
+//change difficulty button
 const mortalChangeDifficulty = document.getElementById("mortal-change-btn")
 const demiChangeDifficulty = document.getElementById("demi-change-btn")
 const olympianChangeDifficulty = document.getElementById("olympian-change-btn")
+
+//quiz game elements
 const quizGame = document.getElementById("quiz-container")
-let currentQuestion, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById("question")
-const answersOptions = document.getElementsByClassName('answer-btns')
-const nextButton = document.getElementsByClassName("next-btn")
+const nextButton = document.getElementById("next-btn")
+const answers = document.getElementsByName("answer");
 
 //all event listeners
-mortalGame.addEventListener('click', startGameMortal)
-demiGame.addEventListener('click', startGameDemi)
-olympianGame.addEventListener('click', startGameOlympian)
+
+//If difficulty clicked
 mortalDifficulty.addEventListener('click', startMortalRules)
 demiDifficulty.addEventListener('click', startDemiRules)
 olympianDifficulty.addEventListener('click', startOlympianRules)
+
+//If change difficulty clicked
 mortalChangeDifficulty.addEventListener('click', startChangeDifficultyMortal)
 demiChangeDifficulty.addEventListener('click', startChangeDifficultyDemi)
 olympianChangeDifficulty.addEventListener('click', startChangeDifficultyOlympian)
+
+//If start button clicked
+mortalGame.addEventListener('click', startGameMortal)
+demiGame.addEventListener('click', startGameDemi)
+olympianGame.addEventListener('click', startGameOlympian)
+
+//If next button clicked
+nextButton.addEventListener('click', nextButtonClicked)
+    
 
 
 //all functions
@@ -36,6 +57,7 @@ function startMortalRules() {
     mortalDifficulty.classList.add('hide')
     demiDifficulty.classList.add('hide')
     olympianDifficulty.classList.add('hide')
+    difficultyTitle.classList.add('hide')
 }
 
 function startDemiRules() {
@@ -43,6 +65,7 @@ function startDemiRules() {
     mortalDifficulty.classList.add('hide')
     demiDifficulty.classList.add('hide')
     olympianDifficulty.classList.add('hide')
+    difficultyTitle.classList.add('hide')
 }
 
 function startOlympianRules() {
@@ -50,14 +73,16 @@ function startOlympianRules() {
     mortalDifficulty.classList.add('hide')
     demiDifficulty.classList.add('hide')
     olympianDifficulty.classList.add('hide')
+    difficultyTitle.classList.add('hide')
 }
 
-// Goes back to start page to change difficulty when clicked
+// Goes back to difficulty selectors when clicked
 function startChangeDifficultyMortal(){
     mortalRules.classList.add('hide')
     mortalDifficulty.classList.remove('hide')
     demiDifficulty.classList.remove('hide')
     olympianDifficulty.classList.remove('hide')
+    difficultyTitle.classList.remove('hide')
 
 }
 
@@ -66,6 +91,8 @@ function startChangeDifficultyDemi(){
     mortalDifficulty.classList.remove('hide')
     demiDifficulty.classList.remove('hide')
     olympianDifficulty.classList.remove('hide')
+    difficultyTitle.classList.remove('hide')
+
 }
 
 function startChangeDifficultyOlympian(){
@@ -73,13 +100,14 @@ function startChangeDifficultyOlympian(){
     mortalDifficulty.classList.remove('hide')
     demiDifficulty.classList.remove('hide')
     olympianDifficulty.classList.remove('hide')
+    difficultyTitle.classList.remove('hide')
 }
 
-//starts the specified difficulties quiz when clicked
+//starts the difficulty quiz when clicked
 function startGameMortal() {
     mortalRules.classList.add('hide')
     quizGame.classList.remove('hide')
-    currentQuestion = questionsMortal
+    shuffledQuestions = questionsMortal.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion()  
 }
@@ -87,37 +115,68 @@ function startGameMortal() {
 function startGameDemi() {
     demiRules.classList.add('hide')
     quizGame.classList.remove('hide')
-    currentQuestion = questionsDemi
+    shuffledQuestions = questionsDemi.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    setNextQuestion() 
+    setNextQuestion()  
 
 }
 
 function startGameOlympian() {
     olympianRules.classList.add('hide')
     quizGame.classList.remove('hide')
-    currentQuestion = questionsOlympian
+    shuffledQuestions = questionsOlympian.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion() 
 }
 
+//Loads question when start button and next button clicked
 function setNextQuestion() {
-    showQuestion(currentQuestion[currentQuestionIndex])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(currentQuestion) {
-    questionElement.innerText = currentQuestion.question
-    document.getElementById("answer-a").innerHTML = currentQuestion.answerA;
-    document.getElementById("answer-b").innerHTML = currentQuestion.answerB;
-    document.getElementById("answer-c").innerHTML = currentQuestion.answerC;
-    document.getElementById("answer-d").innerHTML = currentQuestion.answerD;
-
-    
-
+//Shows question and answer options
+function showQuestion(shuffledQuestions) {
+    questionElement.innerText = shuffledQuestions.question
+    document.getElementById("answer-a").innerHTML = shuffledQuestions.answerA;
+    document.getElementById("answer-b").innerHTML = shuffledQuestions.answerB;
+    document.getElementById("answer-c").innerHTML = shuffledQuestions.answerC;
+    document.getElementById("answer-d").innerHTML = shuffledQuestions.answerD;
+    if (answers.correct) {
+        answers.dataset.correct = answers.correct
+    }
+    answers.addEventListener('click', selectAnswer)
 }
 
-function selectAnswer() {
+// hides the next button 
+function resetState() {
+    nextButton.classList.add('hide')
+}
 
+//
+function selectAnswer(e) {
+    const selectedButton = e.startGameMortal
+    const correct = selectedButton.dataset.correct
+    Array.from(answers).forEach(answer => {
+        setStatusClass(answer, answer.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex +1){
+        nextButton.classList.remove('hide') 
+    } 
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 function nextButtonClicked() {
